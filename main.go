84f877c8d4a644ff77bd0873751feee60c2cb21a
Andy Chao <zhaoqing@spon.com.cn>
2024-07-03 16:16:43 +0800

    完成资源管理页面

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"math"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -60,7 +61,18 @@ func initMinio() {
 	}
 }
 
-func uploadFile(w http.ResponseWriter, r *http.Request) {
+func isValidFileType(filename string) bool {
+	validExtensions := []string{".mp3", ".wav"}
+	ext := strings.ToLower(filepath.Ext(filename))
+	for _, validExt := range validExtensions {
+		if ext == validExt {
+			return true
+		}
+	}
+	return false
+}
+
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	// 设置 CORS 头
 	w.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有来源，或者指定具体的来源
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
@@ -72,41 +84,68 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 如果是其他方法，则返回方法不允许的错误
-	http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-
-	r.ParseMultipartForm(10 << 20)
-	file, handler, err := r.FormFile("file")
+	err := r.ParseMultipartForm(50 << 20) // 限制上传文件的大小为50MB
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
+		http.Error(w, "Error parsing form data", http.StatusBadRequest)
 		return
 	}
-	defer file.Close()
 
-	_, err = minioClient.PutObject(context.Background(), bucketName, handler.Filename, file, handler.Size, minio.PutObjectOptions{ContentType: handler.Header.Get("Content-Type")})
-	if err != nil {
-		fmt.Println(err)
+	files := r.MultipartForm.File["files"]
+	filePaths := r.MultipartForm.Value["filePath"]
+
+	for i, fileHeader := range files {
+		if !isValidFileType(fileHeader.Filename) {
+			http.Error(w, "Invalid file type. Only mp3 and wav are allowed", http.StatusBadRequest)
+			return
+		}
+
+		file, err := fileHeader.Open()
+		if err != nil {
+			http.Error(w, "Error opening file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		// 使用传递的路径或默认路径
+		filePath := "uploads/"
+		if len(filePaths) > i {
+			filePath = filePaths[i]
+		}
+
+		_, err = minioClient.PutObject(context.Background(), bucketName, filePath+fileHeader.Filename, file, fileHeader.Size, minio.PutObjectOptions{ContentType: fileHeader.Header.Get("Content-Type")})
+		if err != nil {
+			http.Error(w, "Error uploading file", http.StatusInternalServerError)
+			return
+		}
 	}
-	fmt.Fprintf(w, "File uploaded successfully\n")
+
+	fmt.Fprintf(w, "Files uploaded successfully\n")
 }
 
-func downloadFile(w http.ResponseWriter, r *http.Request) {
-	// 设置 CORS 头
+func downloadFileHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有来源，或者指定具体的来源
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	if r.Method != http.MethodGet {
+	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	vars := mux.Vars(r)
-	filename := vars["filename"]
+	type downloadFileRequest struct {
+		Key string `json:"key"`
+	}
 
-	object, err := minioClient.GetObject(context.Background(), bucketName, filename, minio.GetObjectOptions{})
+	var request downloadFileRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	object, err := minioClient.GetObject(context.Background(), bucketName, request.Key, minio.GetObjectOptions{})
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -120,6 +159,7 @@ func deleteFileHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有来源，或者指定具体的来源
 	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	if r.Method != http.MethodDelete {
@@ -127,44 +167,44 @@ func deleteFileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	vars := mux.Vars(r)
-	objectName := vars["object"]
+	// 解析 JSON 请求体
+	var requestBody struct {
+		KeyList []string `json:"keyList"`
+	}
 
-	err := minioClient.RemoveObject(context.Background(), bucketName, objectName, minio.RemoveObjectOptions{})
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
 		return
 	}
+
+	// 批量删除对象
+	for _, objectName := range requestBody.KeyList {
+		err := minioClient.RemoveObject(context.Background(), bucketName, objectName, minio.RemoveObjectOptions{})
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to delete object %s: %s", objectName, err.Error()), http.StatusInternalServerError)
+			return
+		}
+	}
+
 	w.WriteHeader(http.StatusNoContent)
 }
 
 type ObjectInfo struct {
-	FileName     string       `json:"fileName"`
-	Key          string       `json:"key"`
-	IsDir        bool         `json:"isDir"`
-	Size         float64      `json:"size"`
-	LastModified string       `json:"lastModified"`
-	Children     []ObjectInfo `json:"children,omitempty"`
-	IsRoot       bool         `json:"isRoot"`
-	ParentKey    string       `json:"parentKey,omitempty"`
+	FileName     string  `json:"fileName"`
+	Key          string  `json:"key"`
+	IsDir        bool    `json:"isDir"`
+	Size         float64 `json:"size"`
+	LastModified string  `json:"lastModified"`
 }
 
-func buildTree(client *minio.Client, bucket, prefix string) ObjectInfo {
+func buildResourceList(client *minio.Client, bucket, prefix string) []ObjectInfo {
 	opts := minio.ListObjectsOptions{
 		Recursive: false,
 		Prefix:    prefix,
 	}
 
 	objectCh := client.ListObjects(context.Background(), bucket, opts)
-
-	root := ObjectInfo{
-		FileName:  "文件列表", // 移除前缀的斜杠
-		Key:       prefix,
-		IsDir:     strings.HasSuffix(prefix, "/"),
-		Children:  []ObjectInfo{},
-		IsRoot:    true,
-		ParentKey: "",
-	}
 
 	// 用于存储目录和文件的切片
 	var dirs []ObjectInfo
@@ -179,60 +219,78 @@ func buildTree(client *minio.Client, bucket, prefix string) ObjectInfo {
 		// 检查是否为目录
 		isDir := strings.HasSuffix(object.Key, "/")
 		name := strings.TrimPrefix(object.Key, prefix)
+
 		if isDir {
-			// 递归构建子树
-			child := buildTree(client, bucket, object.Key)
-			child.FileName = strings.TrimSuffix(name, "/")
-			child.ParentKey = prefix // 设置父节点的 Key
-			dirs = append(dirs, child)
+			name = strings.TrimSuffix(name, "/")
+		}
+
+		// 过滤掉 fileName 为空的数据
+		if name == "" {
+			continue
+		}
+
+		// 添加文件或目录到相应的切片
+		sizeMB := float64(object.Size) / (1024 * 1024) // 转换为MB
+		info := ObjectInfo{
+			FileName:     name,
+			Key:          object.Key,
+			IsDir:        isDir,
+			Size:         math.Round(sizeMB*100) / 100, // 保留两位小数
+			LastModified: object.LastModified.Format("2006-01-02 15:04:05"),
+		}
+
+		if isDir {
+			dirs = append(dirs, info)
 		} else {
-			// 添加文件到当前目录
-			sizeMB := float64(object.Size) / (1024 * 1024) // 转换为MB
-			info := ObjectInfo{
-				FileName:     name,
-				Key:          object.Key,
-				IsDir:        isDir,
-				Size:         math.Round(sizeMB*100) / 100, // 保留两位小数
-				LastModified: object.LastModified.Format("2006-01-02 15:04:05"),
-				ParentKey:    prefix, // 设置父节点的 Key
-			}
 			files = append(files, info)
 		}
 	}
 
 	// 合并目录和文件，确保目录在前
-	root.Children = append(dirs, files...)
+	items := append(dirs, files...)
 
-	return root
+	return items
 }
 
 // 获取文件资源树
-func getResourceTreeHanlder(w http.ResponseWriter, r *http.Request) {
+func getResourceListHanlder(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有来源，或者指定具体的来源
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusOK)
 
-	if r.Method != http.MethodGet {
+	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// 从查询参数中获取comID
-	comID := r.URL.Query().Get("comID")
-	if comID == "" {
-		http.Error(w, "Missing comID parameter", http.StatusBadRequest)
+	type GetResourceListRequest struct {
+		Path  string `json:"path"`
+		ComID string `json:"comID"`
+	}
+
+	var request GetResourceListRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	// 假设comID用于构建bucket名称和路径
-	prefix := comID + "/resource/"
+	var prefix string
+	if request.Path == "" {
+		prefix = request.ComID + "/resource/"
+	} else {
+		prefix = request.Path
+	}
 
-	// 构建树形结构
-	resourceTree := buildTree(minioClient, bucketName, prefix)
+	// 构建资源列表
+	resourceList := buildResourceList(minioClient, bucketName, prefix)
 
 	// 将树形结构转换为JSON格式
-	jsonTree, err := json.MarshalIndent(resourceTree, "", "  ")
+	jsonTree, err := json.MarshalIndent(resourceList, "", "  ")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -271,17 +329,63 @@ func previewFileHandler(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, obj)
 }
 
+// 新建文件夹
+func createFolderHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有来源，或者指定具体的来源
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	type CreateFolderRequest struct {
+		CurrentPath string `json:"currentPath"`
+		FolderName  string `json:"folderName"`
+	}
+
+	var request CreateFolderRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	currentPath := strings.TrimSuffix(request.CurrentPath, "/") + "/"
+	folderName := strings.TrimSuffix(request.FolderName, "/") + "/"
+	objectName := currentPath + folderName
+
+	// Create a folder by creating an empty object with a trailing slash
+	_, err = minioClient.PutObject(r.Context(), bucketName, objectName, nil, 0, minio.PutObjectOptions{})
+	if err != nil {
+		log.Println("Failed to create folder:", err)
+		http.Error(w, "Failed to create folder", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"success": true,
+		"message": "Folder created successfully",
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	initMinio()
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/upload", uploadFile)
-	router.HandleFunc("/download/{filename}", downloadFile)
-	router.HandleFunc("/delete/{object}", deleteFileHandler)
-
-	router.HandleFunc("/resourceTree", getResourceTreeHanlder)
+	router.HandleFunc("/upload", uploadFileHandler)
+	router.HandleFunc("/download", downloadFileHandler)
+	router.HandleFunc("/delete", deleteFileHandler)
+	router.HandleFunc("/resourceList", getResourceListHanlder)
 	router.HandleFunc("/previewFile", previewFileHandler)
+	router.HandleFunc("/createFolder", createFolderHandler)
 
 	// 静态文件服务
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("/static")))
